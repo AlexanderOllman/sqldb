3f1637bcff9456d2493880f76357f2be31c69e70
AlexanderOllman <alexander.ollman@gmail.com>
2024-06-02 11:35:24 -0700

    Adding build bash script

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Pulling latest..."
 git pull
+
+echo "Building image..."
 docker build -t fheonix/rag-app:0.0.3 .
+
+echo "Pushing image..."
 docker push fheonix/rag-app:0.0.3
+
+echo "Done."
