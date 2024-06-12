f14b9cda6530d64e9dd28314b67b3595f95684a0
AlexanderOllman <alexander.ollman@gmail.com>
2024-06-11 17:39:24 -0700

    forgot to change the port

--- src/app.py
+++ src/app.py
@@ -1023,4 +1023,5 @@ with gr.Blocks(theme=theme) as demo:
         )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=8080, allowed_paths=["."])
+    # demo.launch()
